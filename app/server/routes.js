e10cedbb8691eb37c605d6980c2c332ff84ead3e
Himon-SYNCRAFT <d.zawlocki@syncraft.pl>
2017-01-17 22:41:27 +0100

    Add user authentication using twitter account - server side

--- app/server/routes.js
+++ app/server/routes.js
@@ -1,10 +1,81 @@
 'use strict'
 
 const yelp = require('./yelp')
+const passport = require('passport')
+const TwitterStrategy = require('passport-twitter').Strategy
+
+const TWITTER_CONSUMER_KEY = 'ghDimAJtos2LhBCjjmoXZXIJe'
+const TWITTER_CONSUMER_SECRET = '0trz1qTQw1mD0gQpfkaBAJPSLvc16s8dEDStGvutcDsTWoBe6y'
+const TWITTER_CALLBACK_URL = 'http://localhost:3000/twitter/callback'
+
 const tokenName = 'nightlife'
 
+let Twitter
+
 module.exports = (app, db) => {
-    let tokensCollection = db.collection('tokens')
+
+    const tokensCollection = db.collection('tokens')
+    const usersCollection = db.collection('users')
+
+    passport.use(new TwitterStrategy(
+        {
+            consumerKey: TWITTER_CONSUMER_KEY,
+            consumerSecret: TWITTER_CONSUMER_SECRET,
+            callbackURL: TWITTER_CALLBACK_URL
+        },
+
+        (token, tokenSecret, profile, done) => {
+            usersCollection.findOne({'twitterId': profile.id}, (err, user) => {
+                const data = {
+                    name: profile.username,
+                    twitterId: profile.id,
+                }
+
+                if (!user) {
+                    usersCollection.insertOne({ name: profile.name, twitterId: profile.id }, (err, doc) => {
+                        if (doc) {
+                            data._id = doc.insertedId
+                        }
+                        done(err, data)
+                    })
+                } else {
+                    done(err, user)
+                }
+            })
+        }
+    ))
+
+    passport.serializeUser((user, done) => {
+        const data = {
+            name: user.username,
+            twitterId: user.id,
+        }
+
+        done(null, data)
+    })
+
+    passport.deserializeUser((data, done) => {
+
+        usersCollection.findOne({'twitterId': data.twitterId}, (err, user) => {
+            if (!user) {
+                usersCollection.insertOne({ name: data.name, twitterId: data.twitterId }, (err, doc) => {
+                    if (doc) {
+                        data._id = doc.insertedId
+                    }
+                    done(err, data)
+                })
+            } else {
+                done(err, user)
+            }
+        })
+    })
+
+    app.get('/auth/twitter', passport.authenticate('twitter'))
+
+    app.get('/twitter/callback', passport.authenticate('twitter', {
+        successRedirect: '/',
+        failureRedirect: '/loginFailed'
+    }))
 
     app.route('/api/search/:location/')
         .get((req, res) => {
--- server.js
+++ server.js
@@ -3,6 +3,8 @@
 const express = require('express')
 const mongo = require('mongodb').MongoClient
 const routes = require('./app/server/routes.js')
+const session = require('express-session')
+const passport = require('passport')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -15,7 +17,16 @@ mongo.connect(connectionString, (err, db) => {
         console.log('Connected to DB on port 51137')
     }
 
+    app.use(session({
+        secret: '$2a$12$2Z.wdo.8ytoNn6b5faNAt.ywUFo5g2BmbS2FBJAUbg2iUWJc7li9q',
+        resave: true,
+        saveUninitialized: false
+    }))
+
     app.use('/public', express.static(process.cwd() + '/public'))
+
+    app.use(passport.initialize())
+    app.use(passport.session())
 
     routes(app, db)
 
